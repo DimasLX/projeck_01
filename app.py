b2182fdf8bbd24912daccfc254518422634c33a9
dimas <dimasramadani1982@gmail.com>
2023-09-05 18:40:10 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from datetime import datetime
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+app = Flask(__name__)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    articles = list(db.diary.find({}, {'_id': False}))
+    return jsonify({'articles': articles})
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    title_receive = request.form.get('title_give')
+    content_receive = request.form.get('content_give')
+
+    file = request.files['file_give']
+    extension = file.filename.split('.')[-1]
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    filename = f'static/post-{mytime}.{extension}'
+    file.save(filename)
+
+    doc = {
+        'file' :filename,
+        'title': title_receive,
+        'content': content_receive 
+    }
+    db.diary.insert_one(doc)
+    return jsonify({'message': 'DATA TERSIMPAN'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
